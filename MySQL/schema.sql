97300bb3ec0067bd3dc71eb100e14fa851b6cce3
Rihan <49393874+rihan7@users.noreply.github.com>
2020-07-24 11:12:56 +0600

    update schema file

--- MySQL/schema.sql
+++ MySQL/schema.sql
@@ -14,17 +14,3 @@ create table users (
 (255) UNIQUE
    )
 
-
-UPDATE users SET google_id='22skfjsdkk', google_email='rihan3011@gmail.com' WHERE email = 'rihan3011@gmail.com';
-
-
-SELECT *
-from users
-WHERE email = 'rihan301@gmail.com' AND google_id = '1m1m1m11m ';
-
-
-UPDATE users SET google_id = '112292685882967849011', google_email = 'rihan.taher@gmail.com WHERE
-(SELECT * FROM users WHERE  email = 'rihan.taher@gmail.com'|| google_email = '
-rihan.taher@gmail.com' || facebook_email = 'rihan.taher@gmail.com')
-
-UPDATE users SET google_id = '112292685882967849011', google_email = 'rihan.taher@gmail.com WHERE email = 'rihan.taher@gmail.com'|| google_email = 'rihan.taher@gmail.com' || facebook_email = 'rihan.taher@gmail.com';
